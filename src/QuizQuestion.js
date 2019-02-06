5bf4cea396c1c5840f5ee17ef1ea64a10a3f74bc
Alexandre Torres Sampaio <at.sampaio@gmail.com>
2019-02-06 13:27:19 -0200

    Creating a QuizQuestionButton Component

--- src/QuizQuestion.js
+++ src/QuizQuestion.js
@@ -5,20 +5,20 @@ let quizData = require('./quiz_data.json')
 
 class QuizQuestion extends Component {
 
-  render() {
-    return (
-        <main>
-        <section>
-          <p>{this.props.quiz_question.instruction_text}</p>
-        </section>
-        <section className="buttons">
-          <ul>
-            <QuizQuestionButton button_text={this.props.quiz_question.answer_options[0]} />
-          </ul>
-        </section>
-      </main>
-    )
-  }
+    render() {
+        return (
+            <main>
+                <section>
+                    <p>{this.props.quiz_question.instruction_text}</p>
+                </section>
+                <section className="buttons">
+                    <ul>
+                        <QuizQuestionButton button_text={this.props.quiz_question.answer_options[0]} />
+                    </ul>
+                </section>
+            </main>
+        )
+    }
 }
 
 export default QuizQuestion
